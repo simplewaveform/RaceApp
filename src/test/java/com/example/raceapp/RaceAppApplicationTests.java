431e61405257613818e7f464a7a52f3b8f5a4013
simplewaveform <ser1218kzs@gmail.com>
2025-02-10 13:23:18 +0300

    initial commit

---
+++ src/test/java/com/example/raceapp/RaceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
