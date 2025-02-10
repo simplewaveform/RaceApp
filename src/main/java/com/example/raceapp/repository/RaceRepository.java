adf5b729dc0c0ce54033ad5065adb0bc73a0205c
simplewaveform <ser1218kzs@gmail.com>
2025-02-10 18:24:11 +0300

    repo dummy code

---
+++ src/main/java/com/example/raceapp/repository/RaceRepository.java
@@ -0,0 +1,12 @@
+/*
+package com.example.raceapp.repository;
+
+import com.example.raceapp.model.Race;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RaceRepository extends JpaRepository<Race, Integer> {
+    // Пока пустой репозиторий, но его можно будет расширить, например, для поиска по имени или году
+}
+*/
