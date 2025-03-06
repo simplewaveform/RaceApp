431395d6aaebd669b90eb162c85c9b87df42dc3a
simplewaveform <ser1218kzs@gmail.com>
2025-03-07 01:34:26 +0300

    minor cleanup

--- src/main/java/com/example/raceapp/model/Race.java
+++ src/main/java/com/example/raceapp/model/Race.java
@@ -1,9 +1,7 @@
 package com.example.raceapp.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
