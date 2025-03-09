73a2f7aac0067e14906ec9ec0332b61541cb978a
simplewaveform <ser1218kzs@gmail.com>
2025-03-10 01:06:46 +0300

    unused import removed

--- src/main/java/com/example/raceapp/model/Pilot.java
+++ src/main/java/com/example/raceapp/model/Pilot.java
@@ -2,7 +2,6 @@ package com.example.raceapp.model;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
